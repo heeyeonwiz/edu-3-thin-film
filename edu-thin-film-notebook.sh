df1291b1075353bb4891e9020ded0f4f2dd295af
starcell npark <npark@starcell.co.kr>
2021-02-17 14:10:04 +0900

    sh script 추가

---
+++ edu-thin-film-notebook.sh
@@ -0,0 +1,4 @@
+## notebook-edu-thin-film.sh
+cp /home/deepcell/edu-3-thin-film/*.ipynb /home/deepcell/notebook/6.thin-film/
+rm -rf /home/deepcell/edu-3-thin-film/
+ls -al /home/deepcell/notebook/6.thin-film/
